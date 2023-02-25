2787990de2ce7c21cd588564818b2df7a03f293e
AhmedOsama198 <ahmed.osama1982002@gmail.com>
2023-02-25 14:51:33 +0200

    first commit

---
+++ src/components /Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      Navbar
+    </nav>
+  )
+}
+
+export default Navbar
