da947e274089eb09ffc77e12bc903b5fe707bf4d
AhmedOsama198 <ahmed.osama1982002@gmail.com>
2023-02-25 15:52:32 +0200

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Navbar from './components /Navbar'
+import styles from "./style";
 
 function App() {
   return (
-    <div>
-      
+    <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
     </div>
   )
 }
--- src/components /Navbar.jsx
+++ src/components /Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import { navLinks } from '../constants'
+import {close , logo , menu} from '../assets'
+
+
 function Navbar() {
+    console.log(navLinks)
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
-      Navbar
+      <img src={logo} alt = "addthistocc" />
+    <ul className='list-none sm:flex hidden justify-end otems-center flex-1'>
+        {navLinks.map((navLink)=>{
+            <li className='mr-10 text-[16px] font-poppins font-normal cursor-pointer text-white' > 
+                key = {navLink.id}
+                <a href={`#$navLink.id`} > 
+                {navLink.title}
+                </a>
+            </li>
+        })}
+    </ul>
     </nav>
   )
 }
